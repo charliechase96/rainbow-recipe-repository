fdbcebafc6591ee621a2993610ecf32be7c2726b
Charlie Gray <charliechasegray@gmail.com>
2023-12-19 12:41:04 -0800

    add email/password auth; add Links to Signup and Login components

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { auth, googleAuthProvider } from '../firebase';
-import { signInWithPopup } from 'firebase/auth';
+import { signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
+import SignUp from './SignUp';
 
 function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        console.log('Login successful', userCredential);
+        // Redirect or update UI
+      })
+      .catch((error) => {
+        console.error('Error logging in:', error.message);
+        // Handle errors here
+      });
+  };
 
   function signInWithGoogle() {
     signInWithPopup(auth, googleAuthProvider)
@@ -21,8 +39,27 @@ function Login() {
 
   return (
     <div>
-        <h3>Login with Google</h3>
-        <button onClick={signInWithGoogle}>Login with Google</button>
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <input 
+          type="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          placeholder="Email"
+        />
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+      
+      <Link to="/signup" element={<SignUp />}>New user? Create an account!</Link>
+
+      <h3>Login with Google</h3>
+      <button onClick={signInWithGoogle}>Login with Google</button>
     </div>
   );
 }
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,12 +1,47 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
+import Login from './Login';
 
 function SignUp() {
-  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        console.log('Sign up successful', userCredential);
+        // Redirect or update UI
+      })
+      .catch((error) => {
+        console.error('Error signing up:', error.message);
+        // Handle errors here
+      });
+  };
 
   return (
-   <p></p>
+    <div>
+      <form onSubmit={handleSignUp}>
+        <h1>Sign Up</h1>
+        <input 
+          type="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          placeholder="Email"
+        />
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Password"
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+      <Link to="/" element={<Login />}>Already have an account? Log In.</Link>
+    </div>
   );
 }
 
