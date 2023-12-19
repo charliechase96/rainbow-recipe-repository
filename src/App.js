3572a47c89d1525bce00b8062d526d6568f0515c
Charlie Gray <charliechasegray@gmail.com>
2023-12-18 22:42:16 -0800

    add Firebase to project, add SignIn.js component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC47TQQVuMd7wNB6xF2SYNhVJM6VZ0Ems8",
+    authDomain: "recipe-app-charliechase96.firebaseapp.com",
+    projectId: "recipe-app-charliechase96",
+    storageBucket: "recipe-app-charliechase96.appspot.com",
+    messagingSenderId: "1056025742449",
+    appId: "1:1056025742449:web:b0e22c86e90934656c5fd0",
+    measurementId: "G-JS057YYK74"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p></p>
   );
 }
 
---
+++ src/SignUp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { auth } from './firebase';
+
+function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function register(e) {
+    e.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password)
+      .then((authUser) => console.log(authUser))
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div>
+      <form onSubmit={register}>
+        <h1>Sign Up</h1>
+        <input 
+            type="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} placeholder="Email"
+        />
+        <input 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} placeholder="Password"
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
