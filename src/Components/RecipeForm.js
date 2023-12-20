89a169cfc2004e5a88493fd014bfb9fab23fb1e7
Charlie Gray <charliechasegray@gmail.com>
2023-12-19 16:44:27 -0800

    combine handleSubmit and handleRecipePost functions and logic into one function

--- src/Components/RecipeForm.js
+++ src/Components/RecipeForm.js
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useId } from "react";
 import { useNavigate } from "react-router-dom";
+import { db } from '../firebase';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
 
 function RecipeForm() {
     const [recipeName, setRecipeName] = useState('');
@@ -7,14 +10,8 @@ function RecipeForm() {
     const [recipes, setRecipes] = useState([]);
 
     const navigate = useNavigate();
+    const id = useId();
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        const newRecipe = { name: recipeName, servings: servings };
-        setRecipes([...recipes, newRecipe]);
-        setRecipeName('');
-        setServings();
-    };
 
     function handleDeleteRecipe(recipeIndex) {
         setRecipes(recipes.filter((index) => index !== recipeIndex));
@@ -24,9 +21,89 @@ function RecipeForm() {
         navigate('/ingredients-list')
     };
 
+    function getIdToken() {
+        return new Promise((resolve, reject) => {
+          getAuth().onAuthStateChanged(user => {
+            if (user) {
+              user.getIdToken().then(idToken => {
+                resolve(idToken);
+              });
+            } else {
+              reject('No user logged in');
+            }
+          });
+        });
+      }
+    
+    // function postNewRecipe(newRecipe) {
+    //     const url = `https://firestore.googleapis.com/v1/projects/recipe-app-charliechase96/databases/(default)/documents/recipes`;
+
+    //     getIdToken().then(idToken => {
+    //         fetch(url, {
+    //             method: 'POST',
+    //             headers: { 
+    //                 'Authorization': `Bearer ${idToken}`,
+    //                 'Content-Type': 'application/json'},
+    //             body: JSON.stringify({
+    //                 name: recipeName,
+    //                 servings: servings,
+    //                 ingredients: {}
+    //             })
+    //         .then(response => response.json())
+    //         .then(data => console.log('Recipe created:', data))
+    //         .catch(error => console.error('Error creating recipe:', error))
+    //         })
+    //     })
+    // };
+
+    function handleRecipePost(e) {
+        e.preventDefault();
+    
+        // Prepare new recipe data
+        const newRecipe = { name: recipeName, servings: servings };
+    
+        // URL for Firestore
+        const url = `https://firestore.googleapis.com/v1/projects/recipe-app-charliechase96/databases/(default)/documents/recipes`;
+    
+        // Get ID token
+        getAuth().onAuthStateChanged(user => {
+            if (user) {
+                user.getIdToken().then(idToken => {
+                    // Make the POST request with ID token
+                    fetch(url, {
+                        method: 'POST',
+                        headers: { 
+                            'Authorization': `Bearer ${idToken}`,
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ recipe: formatRecipeData(newRecipe) })
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log('Recipe created:', data);
+                        setRecipes([...recipes, newRecipe]); // Update state with new recipe
+                        setRecipeName(''); // Reset recipe name
+                        setServings(0); // Reset servings
+                    })
+                    .catch(error => console.error('Error creating recipe:', error));
+                });
+            } else {
+                console.error('No user logged in');
+            }
+        });
+    }
+
+    function formatRecipeData(recipe) {
+        recipe = {  id: id,
+                    name: recipeName,
+                    servings: servings,
+                    ingredients: {}
+        }
+    }
+
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleRecipePost}>
                 <label>Recipe Name</label>
                 <input 
                     type="text" 
--- src/Components/RecipesHome.js
+++ src/Components/RecipesHome.js
@@ -1,52 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import LogoutButton from "./LogoutButton";
-import { db } from '../firebase';
-import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { getAuth } from "firebase/auth";
 import RecipeForm from "./RecipeForm";
 
 function RecipesHome() {
-    const [recipes, setRecipes] = useState([]);
-    const [selectedRecipe, setSelectedRecipe] = useState(null);
-    const [showCreateForm, setShowCreateForm] = useState(false);
-
-    function handleRecipeSelect(recipe) {
-        setSelectedRecipe(recipe);
-    };
-
-    function handleRemoveIngredient(recipeId, ingredientId) {
-
-    };
-
-    function getIdToken() {
-        return new Promise((resolve, reject) => {
-          getAuth().onAuthStateChanged(user => {
-            if (user) {
-              user.getIdToken().then(idToken => {
-                resolve(idToken);
-              });
-            } else {
-              reject('No user logged in');
-            }
-          });
-        });
-      }
-    
-    function handleCreateNewRecipe(newRecipe) {
-        const url = `https://firestore.googleapis.com/v1/projects/recipe-app-charliechase96/databases/(default)/documents/recipes`;
-
-        getIdToken().then(idToken => {
-            fetch(url, {
-                method: 'POST',
-                headers: { 
-                    'Authorization': `Bearer ${idToken}`,
-                    'Content-Type': 'application/json'},
-                body: JSON.stringify({
-
-                })
-            })
-        })
-    };
 
     return (
         <>
